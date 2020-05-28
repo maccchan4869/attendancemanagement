e1a19a3d922243645807a157d25d9919c10ecbf7
ymatsuo554 <ymatsuo554@gmail.com>
2020-05-28 23:53:12 +0900

    Ajax通信ファイル追加

---
+++ source/Web/Scripts/Common/Common.js
@@ -0,0 +1,38 @@
+﻿var Common;
+var CommonConst;
+
+// 共通関数.
+$(function () {
+    Common = {
+
+        /** 
+         * POST関数.
+         * 
+         * @param {Object} Url       送信先URL
+         * @param {Object} Parameter パラメータ
+         * @param {Object} onSuccess 成功時のコールバック関数
+         * @param {Object} onError   失敗時のコールバック関数
+         */
+        Post: function (Url, Parameter, onSuccess, onError) {
+            $.ajax({
+                type: "POST",
+                url: Url,
+                data: JSON.stringify(Parameter),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (jsonData) {
+                    onSuccess(jsonData.result);
+                },
+                error: function () {
+                    onError(jsonData);
+                },
+                complete: function () {
+
+                }
+            });
+        }
+    };
+
+    CommonConst = {
+    };
+});
