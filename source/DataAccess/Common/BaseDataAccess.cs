fac6b35b79f1b09b42cfbf1bdd4b49f6c876f29d
ymatsuo554 <ymatsuo554@gmail.com>
2020-05-28 23:39:52 +0900

    一時保存

---
+++ source/DataAccess/Common/BaseDataAccess.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DataAccess.Common
+{
+    public abstract class BaseDataAccess
+    {
+        /// <summary>
+        /// SELECT文.
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public SqlDataReader DBSelect(string sql, List<SqlParameter> param)
+        {
+            SqlDataReader reader;
+            var con = OpenDbConnection();
+            using (var command = con.CreateCommand())
+            {
+                try
+                {
+                    // データベースの接続開始
+                    con.Open();
+
+                    // SQLの設定
+                    command.CommandText = sql;
+                    foreach (SqlParameter row in param)
+                    {
+                        command.Parameters.Add(row);
+                    }
+
+                    // SQLの実行
+                    reader = command.ExecuteReader();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    throw;
+                }
+                return reader;
+            }
+        }
+
+        /// <summary>
+        /// INSERT文.
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="param"></param>
+        public void DBInsert(string sql, List<SqlParameter> param)
+        {
+            var con = OpenDbConnection();
+            using (var command = con.CreateCommand())
+            {
+                try
+                {
+                    // データベースの接続開始
+                    con.Open();
+
+                    // SQLの準備
+                    command.CommandText = sql;
+                    foreach (SqlParameter row in param)
+                    {
+                        command.Parameters.Add(row);
+                    }
+
+                    // SQLの実行
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 接続を取得する
+        /// </summary>
+        /// <param name="dbName"></param>
+        /// <returns></returns>
+        public static SqlConnection OpenDbConnection(string dbName = "Database")
+        {
+            var con = new SqlConnection
+            {
+                ConnectionString = ConfigurationManager.ConnectionStrings[dbName].ConnectionString
+            };
+            return con;
+        }
+    }
+}
