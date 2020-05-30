496998376586ee1a1f0446c1d6a838631f6cd987
ymatsuo554 <ymatsuo554@gmail.com>
2020-05-31 00:30:06 +0900

    スタイル修正

--- source/Web/Scripts/M02/M02.js
+++ source/Web/Scripts/M02/M02.js
@@ -1,1 +1,23 @@
 ﻿
+var M02 = null;
+
+$(function () {
+
+    M02 = {
+
+        /**
+         * 読込完了時イベント
+         */
+        OnReady: function () {
+            $("#common-Nav").css("visibility", "visible");
+        },
+
+        /**
+         * イベント設定.
+         */
+        SetEvent: function () {
+        },
+    };
+
+    M02.OnReady();
+});
