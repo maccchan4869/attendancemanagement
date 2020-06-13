d83692a6c7aaa74d92c79aa62cbe5a883401bb20
ymatsuo554 <ymatsuo554@gmail.com>
2020-06-13 20:28:56 +0900

    不足修正

---
+++ src/ViewModel/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "電子メール")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "パスワード")]
+        public string Password { get; set; }
+
+        [Display(Name = "このアカウントを記憶する")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
